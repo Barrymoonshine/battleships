1689fbdd471b27daed9461aaed370f3292e6c6a1
Nick Sheward <nickshwrd@gmail.com>
2023-04-25 11:19:24 +0100

    Basic file structure created with Webpack, ESLint and Prettierr installed and configured

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles/global-style.css";
+
+const container = document.getElementById("container");
+
+const webpackTestFuntion = () => {
+  container.innerText = "Hello world";
+};
+
+webpackTestFuntion();
